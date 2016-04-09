df63dd3445c880475f293bec5b0998037991f21a
Ric Allinson <allinson@yahoo-inc.com>
2016-04-08 19:51:36 -0700

    Update to readme

--- Path.java
+++ Path.java
@@ -13,7 +13,7 @@ public class Path {
     // The platform-specific path delimiter, ; or ':'.
     //
     // An example on *nix:
-    // ```
+    // ```java
     // console.log(process.env.PATH)
     // // '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin'
     // 
@@ -21,7 +21,7 @@ public class Path {
     // // returns ['/usr/bin', '/bin', '/usr/sbin', '/sbin', '/usr/local/bin']
     // ```
     // An example on Windows:
-    // ```
+    // ```java
     // console.log(process.env.PATH)
     // // 'C:\Windows\system32;C:\Windows;C:\Program Files\node\'
     //
@@ -33,12 +33,12 @@ public class Path {
     // The platform-specific file separator. '\\' or '/'.
     //
     // An example on *nix:
-    //```
+    //```java
     // 'foo/bar/baz'.split(path.sep)
     // // returns ['foo', 'bar', 'baz']
     //```
     // An example on Windows:
-    //```
+    //```java
     // 'foo\\bar\\baz'.split(path.sep)
     // // returns ['foo', 'bar', 'baz']
     //```
@@ -51,7 +51,7 @@ public class Path {
     // Return the last portion of a path. Similar to the Unix basename command.
     //
     // Example:
-    // ```
+    // ```java
     // path.basename('/foo/bar/baz/asdf/quux.html')
     // // returns 'quux.html'
     //
@@ -65,7 +65,7 @@ public class Path {
     // Return the directory name of a path. Similar to the Unix dirname command.
     //
     // Example:
-    // ```
+    // ```java
     // path.dirname('/foo/bar/baz/asdf/quux')
     // // returns '/foo/bar/baz/asdf'
     // ```
@@ -74,7 +74,7 @@ public class Path {
     }
 
     // Return the extension of the path, from the last '.' to end of string in the last portion of the path. If there is no '.' in the last portion of the path or the first character of it is '.', then it returns an empty string. Examples:
-    // ```
+    // ```java
     // path.extname('index.html')
     // // returns '.html'
     //
@@ -106,7 +106,7 @@ public class Path {
     //
     // Example:
     //
-    // ```
+    // ```java
     // path.format({
     //     root : "/",
     //     dir : "/home/user/dir",
@@ -132,7 +132,7 @@ public class Path {
     // Determines whether path is an absolute path. An absolute path will always resolve to the same location, regardless of the working directory.
     //
     // Examples:
-    // ```
+    // ```java
     // Path.isAbsolute('/foo/bar') // true
     // Path.isAbsolute('/baz/..')  // true
     // Path.isAbsolute('qux/')     // false
@@ -150,7 +150,7 @@ public class Path {
     // Arguments must be strings. In v0.8, non-string arguments were silently ignored. In v0.10 and up, an exception is thrown.
     //
     // Example:
-    // ```
+    // ```java
     // path.join('/foo', 'bar', 'baz/asdf', 'quux', '..')
     // // returns '/foo/bar/baz/asdf'
     // ```
@@ -164,7 +164,7 @@ public class Path {
     // When multiple slashes are found, they're replaced by a single one; when the path contains a trailing slash, it is preserved. On Windows backslashes are used.
     //
     // Example:
-    // ```
+    // ```java
     // path.normalize('/foo/bar//baz/asdf/quux/..')
     // // returns '/foo/bar/baz/asdf'
     // ```
@@ -176,7 +176,7 @@ public class Path {
     // Returns an object from a path string.
     //
     // An example on *nix:
-    // ```
+    // ```java
     // path.parse('/home/user/dir/file.txt')
     // // returns
     // // {
@@ -194,7 +194,7 @@ public class Path {
     // Solve the relative path from from to to.
     // 
     // At times we have two absolute paths, and we need to derive the relative path from one to the other. This is actually the reverse transform of path.resolve, which means we see that:
-    // ```
+    // ```java
     // path.resolve(from, path.relative(from, to)) == path.resolve(to)
     // Examples:
     // 
@@ -214,11 +214,11 @@ public class Path {
     // If to isn't already absolute from arguments are prepended in right to left order, until an absolute path is found. If after using all from paths still no absolute path is found, the current working directory is used as well. The resulting path is normalized, and trailing slashes are removed unless the path gets resolved to the root directory. Non-string from arguments are ignored.
     // 
     // Another way to think of it is as a sequence of cd commands in a shell.
-    // ```
+    // ```java
     // path.resolve('foo/bar', '/tmp/file/', '..', 'a/../subfile');
     // ```
     // Is similar to:
-    // ```
+    // ```java
     // cd foo/bar
     // cd /tmp/file/
     // cd ..
@@ -228,7 +228,7 @@ public class Path {
     // The difference is that the different paths don't need to exist and may also be files.
     // 
     // Examples:
-    // ```
+    // ```java
     // path.resolve('/foo/bar', './baz')
     // // returns '/foo/bar/baz'
     // 
--- README.md
+++ README.md
@@ -11,7 +11,7 @@ import github.com.jminusminus.path.Path;
 The platform-specific path delimiter, ; or ':'.
 
 An example on *nix:
-```
+```java
 console.log(process.env.PATH)
 // '/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin'
 
@@ -19,7 +19,7 @@ process.env.PATH.split(path.delimiter)
 // returns ['/usr/bin', '/bin', '/usr/sbin', '/sbin', '/usr/local/bin']
 ```
 An example on Windows:
-```
+```java
 console.log(process.env.PATH)
 // 'C:\Windows\system32;C:\Windows;C:\Program Files\node\'
 
@@ -31,12 +31,12 @@ process.env.PATH.split(path.delimiter)
 The platform-specific file separator. '\\' or '/'.
 
 An example on *nix:
-```
+```java
 'foo/bar/baz'.split(path.sep)
 // returns ['foo', 'bar', 'baz']
 ```
 An example on Windows:
-```
+```java
 'foo\\bar\\baz'.split(path.sep)
 // returns ['foo', 'bar', 'baz']
 ```
@@ -47,7 +47,7 @@ An example on Windows:
 Return the last portion of a path. Similar to the Unix basename command.
 
 Example:
-```
+```java
 path.basename('/foo/bar/baz/asdf/quux.html')
 // returns 'quux.html'
 
@@ -59,14 +59,14 @@ path.basename('/foo/bar/baz/asdf/quux.html', '.html')
 Return the directory name of a path. Similar to the Unix dirname command.
 
 Example:
-```
+```java
 path.dirname('/foo/bar/baz/asdf/quux')
 // returns '/foo/bar/baz/asdf'
 ```
 
 ## static String extname(String p)
 Return the extension of the path, from the last '.' to end of string in the last portion of the path. If there is no '.' in the last portion of the path or the first character of it is '.', then it returns an empty string. Examples:
-```
+```java
 path.extname('index.html')
 // returns '.html'
 
@@ -96,7 +96,7 @@ If the base property is not supplied, a concatenation of the name property and t
 
 Example:
 
-```
+```java
 path.format({
     root : "/",
     dir : "/home/user/dir",
@@ -120,7 +120,7 @@ path.format({
 Determines whether path is an absolute path. An absolute path will always resolve to the same location, regardless of the working directory.
 
 Examples:
-```
+```java
 Path.isAbsolute('/foo/bar') // true
 Path.isAbsolute('/baz/..')  // true
 Path.isAbsolute('qux/')     // false
@@ -136,7 +136,7 @@ Join all arguments together and normalize the resulting path.
 Arguments must be strings. In v0.8, non-string arguments were silently ignored. In v0.10 and up, an exception is thrown.
 
 Example:
-```
+```java
 path.join('/foo', 'bar', 'baz/asdf', 'quux', '..')
 // returns '/foo/bar/baz/asdf'
 ```
@@ -148,7 +148,7 @@ Normalize a string path, taking care of '..' and '.' parts.
 When multiple slashes are found, they're replaced by a single one; when the path contains a trailing slash, it is preserved. On Windows backslashes are used.
 
 Example:
-```
+```java
 path.normalize('/foo/bar//baz/asdf/quux/..')
 // returns '/foo/bar/baz/asdf'
 ```
@@ -158,7 +158,7 @@ Note: If the path string passed as argument is a zero-length string then '.' wil
 Returns an object from a path string.
 
 An example on *nix:
-```
+```java
 path.parse('/home/user/dir/file.txt')
 // returns
 // {
@@ -174,7 +174,7 @@ path.parse('/home/user/dir/file.txt')
 Solve the relative path from from to to.
 
 At times we have two absolute paths, and we need to derive the relative path from one to the other. This is actually the reverse transform of path.resolve, which means we see that:
-```
+```java
 path.resolve(from, path.relative(from, to)) == path.resolve(to)
 Examples:
 
@@ -192,11 +192,11 @@ Resolves to to an absolute path.
 If to isn't already absolute from arguments are prepended in right to left order, until an absolute path is found. If after using all from paths still no absolute path is found, the current working directory is used as well. The resulting path is normalized, and trailing slashes are removed unless the path gets resolved to the root directory. Non-string from arguments are ignored.
 
 Another way to think of it is as a sequence of cd commands in a shell.
-```
+```java
 path.resolve('foo/bar', '/tmp/file/', '..', 'a/../subfile');
 ```
 Is similar to:
-```
+```java
 cd foo/bar
 cd /tmp/file/
 cd ..
@@ -206,7 +206,7 @@ pwd
 The difference is that the different paths don't need to exist and may also be files.
 
 Examples:
-```
+```java
 path.resolve('/foo/bar', './baz')
 // returns '/foo/bar/baz'
 
