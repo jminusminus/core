18f4a7dfe8d00107fb7be671325e8994e062f085
Ric Allinson <allinson@yahoo-inc.com>
2016-04-12 22:36:27 -0700

    Tested readdir

--- FileSystem.java
+++ FileSystem.java
@@ -8,5 +8,20 @@
 package github.com.jminusminus.core;
 
 public class FileSystem {
-	
+
+    // Synchronous readdir(3). Reads the contents of a directory.
+    // Returns an array of the names of the files in the directory excluding '.' and '..'.
+    // If there was an error null is returned.
+    public static String[] readdir(String path) {
+        java.io.File folder = new java.io.File(path);
+        java.io.File[] listOfFiles = folder.listFiles();
+        if (listOfFiles == null) {
+            return null;
+        }
+        String[] files = new String[listOfFiles.length];
+        for (int i = 0; i < listOfFiles.length; i++) {
+            files[i] = listOfFiles[i].getName();
+        }
+        return files;
+    }
 }
--- FileSystem_test.java
+++ FileSystem_test.java
@@ -19,4 +19,17 @@ public class FileSystem_test extends Test {
         FileSystem fs = new FileSystem();
         this.assertEqual("github.com.jminusminus.core.FileSystem", fs.getClass().getName());
     }
+
+    public void test_readdir() {
+        this.should("return an array of files");
+        String[] files = FileSystem.readdir(".");
+        java.util.Arrays.sort(files);
+        this.assertEqual(".git", files[0]);
+    }
+
+    public void test_readdir_bad_path() {
+        this.should("return null as the directory doesn't exist");
+        String[] files = FileSystem.readdir("/foo/bar");
+        this.assertEqual(true, files == null);
+    }
 }
