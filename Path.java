dddafafabb6f54ace355d93f9469a6807485f91b
Ric Allinson <allinson@yahoo-inc.com>
2016-04-14 17:11:55 -0700

    Updated docs

--- Path.java
+++ Path.java
@@ -78,9 +78,9 @@ public class Path {
         return p.substring(0, p.lastIndexOf(Path.sep));
     }
 
-    // Return the extension of the specified path, from the last "." to end of string in the last portion of 
-    // the path. If there is no "." in the last portion of the path or the first character of it 
-    // is ".", then it returns an empty string.
+    // Return the extension of the specified path, from the last `.` to end of string in the last portion of 
+    // the path. If there is no `.` in the last portion of the path or the first character of it 
+    // is `.`, then it returns an empty string.
     //
     // Examples:
     //
@@ -215,7 +215,7 @@ public class Path {
         return path;
     }
 
-    // Normalize the specified path, taking care of ".." and "." parts.
+    // Normalize the specified path, taking care of `..` and `.` parts.
     //
     // When multiple slashes are found, they're replaced by a single one; when the path contains 
     // a trailing slash, it is NOT preserved. On Windows backslashes are used.
@@ -311,13 +311,12 @@ public class Path {
 
     // Resolves to to an absolute path.
     // 
-    // If to isn"t already absolute from arguments are prepended in right to left order, until an absolute path is found. If after using all from paths still no absolute path is found, the current working directory is used as well. The resulting path is normalized, and trailing slashes are removed unless the path gets resolved to the root directory. Non-string from arguments are ignored.
+    // If to isn't already absolute from arguments are pre-pended in right to left order, until an 
+    // absolute path is found. If after using all from paths still no absolute path is found, the 
+    // current working directory is used as well. The resulting path is normalized, and trailing 
+    // slashes are removed unless the path gets resolved to the root directory.
     // 
     // Another way to think of it is as a sequence of cd commands in a shell.
-    //
-    // ```java
-    // Path.resolve("foo/bar", "/tmp/file/", "..", "a/../subfile");
-    // ```
     //
     // Is similar to:
     //
@@ -329,18 +328,18 @@ public class Path {
     // pwd
     // ```
     //
-    // The difference is that the different paths don"t need to exist and may also be files.
+    // The difference is that the different paths don't need to exist and may also be files.
     // 
     // Examples:
     //
     // ```java
-    // Path.resolve("/foo/bar", "./baz")
+    // String p = Path.resolve("/foo/bar", "./baz");
     // // returns "/foo/bar/baz"
     // 
-    // Path.resolve("/foo/bar", "/tmp/file/")
+    // String p = Path.resolve("/foo/bar", "/tmp/file/");
     // // returns "/tmp/file"
     // 
-    // Path.resolve("wwwroot", "static_files/png/", "../gif/image.gif")
+    // String p = Path.resolve("wwwroot", "static_files/png/", "../gif/image.gif");
     // // if currently in /home/myself/node, it returns
     // // "/home/myself/node/wwwroot/static_files/gif/image.gif"
     // ```
@@ -358,7 +357,7 @@ public class Path {
         return Path.join(System.getProperty("user.dir"), path);
     }
 
-    // Resolves . and .. elements in a path array with directory names there
+    // Resolves `.` and `..` elements in a path array with directory names there
     // must be no slashes or device names (c:\) in the array
     // (so also no leading and trailing slashes - it does not distinguish relative and absolute paths).
     protected static String normalizeArray(String[] parts) {
